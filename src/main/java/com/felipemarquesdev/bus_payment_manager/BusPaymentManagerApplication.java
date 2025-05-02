7cfc432c22563891dd45d558489d47ad744f7f11
Antônio Felipe <felipe.marques@animati.com.br>
2025-05-02 14:59:31 -0300

    project creation

---
+++ src/main/java/com/felipemarquesdev/bus_payment_manager/BusPaymentManagerApplication.java
@@ -0,0 +1,13 @@
+package com.felipemarquesdev.bus_payment_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusPaymentManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusPaymentManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felipemarquesdev/bus_payment_manager/BusPaymentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipemarquesdev.bus_payment_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusPaymentManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
