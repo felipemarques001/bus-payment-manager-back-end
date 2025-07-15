e20ff0581009bf15e1cb02f67a34611925b3c7a0
Felipe Marques <felipemarquesgg@outlook.com>
2025-07-14 21:35:41 -0300

    delete(auth): Unnecessary import in AuthServiceImpl

--- src/test/java/com/felipemarquesdev/bus_payment_manager/services/AuthServiceImplTest.java
+++ src/test/java/com/felipemarquesdev/bus_payment_manager/services/AuthServiceImplTest.java
@@ -18,7 +18,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
