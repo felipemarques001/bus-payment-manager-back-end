f43aaec66011bd61a9ceb9f075410e73e3126a02
Antônio Felipe <felipe.marques@animati.com.br>
2025-08-08 07:34:25 -0300

    fix(tuition): fix payment_type mas length

--- src/main/resources/db/migration/V5__create-tuition-table.sql
+++ src/main/resources/db/migration/V5__create-tuition-table.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS TB_TUITION (
     id UUID PRIMARY KEY,
     payment_id UUID REFERENCES TB_PAYMENT(id) ON DELETE CASCADE,
     student_id UUID REFERENCES TB_STUDENT(id),
-    payment_type VARCHAR(10),
+    payment_type VARCHAR(15),
     is_paid BOOLEAN NOT NULL,
     paid_at TIMESTAMP,
     CONSTRAINT payment_type_required_if_is_paid CHECK (NOT(is_paid = true AND payment_type IS NULL)),
